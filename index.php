2cea37780eec8aa7c0a3a63be1ca96480c4e979f
isabella-anhol <88116900+isabella-anhol@users.noreply.github.com>
2021-08-25 19:51:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Hello world!"; //oi
+
+    $nome = 1;
+    $nome2 = 1.0;
+    $nome3 = 1.02;
+    $nome4 = "Sérginho";
+
+    echo " boa tarde, olha: $nome4"; /* olha issoooooooooo */
+
+
+?>
